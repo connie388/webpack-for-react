9d9df46b555b7fded8881278c6ca9330b0b0639f
Connie <sitwailan@gmail.com>
2022-12-29 20:10:38 -0500

    change

--- src/data/dataSlide.js
+++ src/data/dataSlide.js
@@ -1,60 +1,53 @@
 import { v4 as uuidv4 } from "uuid";
-import img1 from "/src/assets/images/img1.jpg";
-import img2 from "/src/assets/images/img2.jpg";
-import img3 from "/src/assets/images/img3.jpg";
-import img4 from "/src/assets/images/img4.jpg";
-import img5 from "/src/assets/images/img5.jpg";
-import img6 from "/src/assets/images/img6.jpg";
-import img7 from "/src/assets/images/img7.jpg";
-import img8 from "/src/assets/images/img8.jpg";
+
 const dataSlide = [
   {
     id: uuidv4(),
     title: "Highest Rated VR Arcade",
     subTitle: "80+ VR experiences and games",
-    img: img1,
+    img: "img1.jpg",
   },
   {
     id: uuidv4(),
     title: "The Top Climbing Gym",
     subTitle: "Drop-in/Classes available",
-    img: img2,
+    img: "img2.jpg",
   },
   {
     id: uuidv4(),
     title: "Indoor Playground",
     subTitle: "Lorem",
-    img: img3,
+    img: "img3.jpg",
   },
   {
     id: uuidv4(),
     title: "Lorem ipsum",
     subTitle: "Lorem",
-    img: img4,
+    img: "img4.jpg",
   },
   {
     id: uuidv4(),
     title: "Lorem ipsum",
     subTitle: "Lorem",
-    img: img5,
+    img: "img5.jpg",
   },
   {
     id: uuidv4(),
     title: "Lorem ipsum",
     subTitle: "Lorem",
-    img: img6,
+    img: "img6.jpg",
   },
   {
     id: uuidv4(),
     title: "Lorem ipsum",
     subTitle: "Lorem",
-    img: img7,
+    img: "img7.jpg",
   },
   {
     id: uuidv4(),
     title: "Lorem ipsum",
     subTitle: "Lorem",
-    img: img8,
+    img: "img8.jpg",
   },
 ];
 export default dataSlide;
--- webpack.config.js
+++ webpack.config.js
@@ -42,7 +42,7 @@ module.exports = {
             options: {
               name: "[name].[ext]",
               outputPath: "/",
-              publicPath: "/",
+              publicPath: "/webpack-for-react/",
             },
           },
         ],
@@ -55,7 +55,7 @@ module.exports = {
             options: {
               name: "[name].[ext]",
               outputPath: "/",
-              publicPath: "/",
+              publicPath: "/webpack-for-react/",
             },
           },
         ],
